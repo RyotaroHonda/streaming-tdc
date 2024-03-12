e0607de22b7ac8ac368b785ca0b9db006ded7a93
Ryotaro Honda <rhonda@post.kek.jp>
2024-03-12 15:00:14 +0900

    Delimiter

--- core/defDelimiter.vhd
+++ core/defDelimiter.vhd
@@ -2,10 +2,9 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 library mylib;
-use mylib.defChannel.all;
-use mylib.defTDC.all;
+use mylib.defHeartBeatUnit.all;
 
-package defDataStructure is
+package defDelimiter is
 
   -- WIDTH of DATA
   constant kWidthData         : integer  := 64;    -- width of the data
@@ -30,7 +29,7 @@ package defDataStructure is
   --constnat kReserve             : integer  := 12;  -- 13th,
   constant kIndexOverflow         : integer  := 11;  -- 12th, IncomingFIFO almost-full or full flags goes high
   constant kIndexGHbfNumMismatch  : integer  := 10;  -- 11th,  Stcp HbfNumber and local HbfNumber mismatch
-  constant kIndexLHbfNumMismatch  : integer  := 9;   -- 10th,  Hbf numbers mismatch in between two mezzanines (Exists only in HR-TDC)
+  constant kIndexLHbfNumMismatch  : integer  := 9;   -- 10th,  Hbf numbers mismatch among merger units
   --constnat kReserve             : integer  := 8;   -- 9th,
   constant kIndexInThrottlingT1   : integer  := 7;  -- 8th,  Input throttling type1 is working
   constant kIndexInThrottlingT2   : integer  := 6;  -- 7th,  Input throttling type2 is working
@@ -44,53 +43,45 @@ package defDataStructure is
   function genFlagVector(index : integer; flag : std_logic) return std_logic_vector;
 
   -- Heartbeat frame, counter ----------------------------------------------------------------
-  constant kWidthHBFrame      : integer  := 24;  -- Heartbeat frame number
-  constant kWidthHBCount      : integer  := 16;  -- Heartbeat counter value
+  constant kWidthStrHbf       : integer  := 24;  -- Heartbeat frame number
+  constant kWidthStrHbc       : integer  := 16;  -- Heartbeat counter value
 
-  constant kMaxHBFrame        : std_logic_vector (kWidthHBFrame-1 downto 0) := X"FFFFFF"; -- heartbeat max frame number
-  constant kMaxHBCount        : std_logic_vector (kWidthHBCount-1 downto 0) := X"FFFF";   -- heartbeat max count number
+  constant kMaxHBFrame        : std_logic_vector (kWidthStrHbf-1 downto 0) := X"FFFFFF"; -- heartbeat max frame number
+  constant kMaxHBCount        : std_logic_vector (kWidthStrHbc-1 downto 0) := X"FFFF";   -- heartbeat max count number
 
   -- Data bit position ---------------------------------------------------------------------
   constant kMSBDataType     : integer  := kWidthData-1;
   constant kLSBDataType     : integer  := kMSBDataType-kWidthDataType+1;
-
-  -- TDC data --
-  constant kMSBChannel      : integer  := kMSBDataType-kWidthDataType;
-  constant kLSBChannel      : integer  := kMSBChannel-kWidthChannel+1;
-  constant kMSBTOT          : integer  := kMSBChannel-kWidthChannel;
-  constant kLSBTOT          : integer  := kMSBTOT-kWidthTOT+1;
-  constant kMSBTiming       : integer  := kMSBTOT-kWidthTOT;
-  constant kLSBTiming       : integer  := kMSBTiming-kWidthTiming+1;
 
   -- Delimiter data --
   -- 1st delimiter: 6-bit data-type, 2-bit reserve, 16-bit flag,    16-bit Time offset,         24-bit HBF number
   -- 2nd delimiter: 6-bit data-type, 2-bit reserve, 16-bit reserve, 20-bit generated ddta size, 20-bit transferred data size
 
   -- 1st delimiter --
-  constant kPosHbdDataType  : std_logic_vector(kWidthData-1          downto kWidthData          -kWidthDataType);
-  constant kPosHbdReserve1  : std_logic_vector(kPosHbdDataType'low-1 downto kPosHbdDataType'low -2);
-  constant kPosHbdFlag      : std_logic_vector(kPosHbdReserve1'low-1 downto kPosHbdReserve1'low -kWidthDelimiterFlag);
-  constant kPosHbdOffset    : std_logic_vector(kPosHbdFlag'low-1     downto kPosHbdFlag'low     -16);
-  constant kPosHbdHBFrame   : std_logic_vector(kPosHbdOffset'low-1   downto kPosHbdOffset'low   -kWidthHBFrame);
+  constant kPosHbdDataType  : std_logic_vector(kWidthData-1          downto kWidthData          -kWidthDataType)      := (others => '0');
+  constant kPosHbdReserve1  : std_logic_vector(kPosHbdDataType'low-1 downto kPosHbdDataType'low -2)                   :=(others => '0');
+  constant kPosHbdFlag      : std_logic_vector(kPosHbdReserve1'low-1 downto kPosHbdReserve1'low -kWidthDelimiterFlag) := (others => '0');
+  constant kPosHbdOffset    : std_logic_vector(kPosHbdFlag'low-1     downto kPosHbdFlag'low     -16)                  := (others => '0');
+  constant kPosHbdHBFrame   : std_logic_vector(kPosHbdOffset'low-1   downto kPosHbdOffset'low   -kWidthStrHbf)       := (others => '0');
 
   -- 2nd delimiter --
-  constant kPosHbdReserve2  : std_logic_vector(kPosHbdReserve1'low-1 downto kPosHbdReserve1'low -16);
-  constant kPosHbdGenSize   : std_logic_vector(kPosHbdReserve2'low-1 downto kPosHbdReserve2'low -20);
-  constant kPosHbdTransSize : std_logic_vector(kPosHbdGenSize'low-1  downto kPosHbdGenSize'low  -20);
+  constant kPosHbdReserve2  : std_logic_vector(kPosHbdReserve1'low-1 downto kPosHbdReserve1'low -16):= (others => '0');
+  constant kPosHbdGenSize   : std_logic_vector(kPosHbdReserve2'low-1 downto kPosHbdReserve2'low -20):= (others => '0');
+  constant kPosHbdTransSize : std_logic_vector(kPosHbdGenSize'low-1  downto kPosHbdGenSize'low  -20):= (others => '0');
 
   function checkDelimiter(data_type : std_logic_vector) return boolean;
 
   -- Data array definition ------------------------------------------------------------------
-  type dFineCountType is array (integer range kNumStrInput-1 downto 0) of std_logic_vector(kWidthFineCount-1 downto 0);
-  type dTimingType    is array (integer range kNumStrInput-1 downto 0) of std_logic_vector(kWidthTiming-1 downto 0);
-  type dTOTType       is array (integer range kNumStrInput-1 downto 0) of std_logic_vector(kWidthTOT-1 downto 0);
-  type dDataType      is array (integer range kNumStrInput-1 downto 0) of std_logic_vector(kWidthData-1 downto 0);
+  --type dFineCountType is array (integer range kNumStrInput-1 downto 0) of std_logic_vector(kWidthFineCount-1 downto 0);
+  --type dTimingType    is array (integer range kNumStrInput-1 downto 0) of std_logic_vector(kWidthTiming-1 downto 0);
+  --type dTOTType       is array (integer range kNumStrInput-1 downto 0) of std_logic_vector(kWidthTOT-1 downto 0);
+  --type dDataType      is array (integer range kNumStrInput-1 downto 0) of std_logic_vector(kWidthData-1 downto 0);
 
-end package defDataStructure;
+end package defDelimiter;
 -- ----------------------------------------------------------------------------------
 -- Package body
 -- ----------------------------------------------------------------------------------
-package body defDataStructure is
+package body defDelimiter is
   function genFlagVector(index : integer; flag : std_logic) return std_logic_vector is
     variable result   : std_logic_vector(kWidthDelimiterFlag-1 downto 0) := (others => '0');
   begin
@@ -107,4 +98,4 @@ package body defDataStructure is
     end if;
   end checkDelimiter;
 
-end package body defDataStructure;
+end package body defDelimiter;
--- core/delimiter/DelimiterGenerator.vhd
+++ core/delimiter/DelimiterGenerator.vhd
@@ -3,9 +3,10 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.numeric_std.ALL;
 
 library mylib;
-use mylib.defDataStructure.all;
+use mylib.defDelimiter.all;
+use mylib.defLaccp.all;
+--use mylib.defODPBlock.all;
 
-use mylib.defODPBlock.all;
 
 entity DelimiterGenerator is
   generic(
@@ -16,16 +17,13 @@ entity DelimiterGenerator is
     rst               : in std_logic;
     clk               : in std_logic;
 
+    -- Status input ----------------------------------
+    flagsIn           : in std_logic_vector(kWidthDelimiterFlag-1 downto 0);
+
     -- LACCP -----------------------------------------
-    hbCount           : in std_logic_vector(kWidthHBCount-1 downto 0);
-    hbfNumber         : in HbNumberType;
-
-    -- HbfFlags --
-    lHbfNumMismatchIn   : in std_logic;
-    gHbfNumMismatchOut  : out std_logic;
-
-    -- Throttling status -----------------------------
-    inThrottlingT2On    : in std_logic;
+    hbCount           : in std_logic_vector(kWidthStrHbc-1 downto 0);
+    hbfNumber         : in std_logic_vector(kWidthStrHbf-1 downto 0);
+    LaccpFineOffset   : in signed(kWidthLaccpFineOffset-1 downto 0);
 
     -- For ODP block ----------------------------------
     -- Delimiter data output --
@@ -39,274 +37,62 @@ architecture RTL of DelimiterGenerator is
   signal sync_reset     : std_logic;
 
   -- Signal decralation ---------------------------------------------
-  signal valid_delimiter      : std_logic;
-  signal final_delimiter_data : std_logic_vector(kWidthData-1 downto 0);
-  signal is_working           : std_logic;
-
-  -- work --
-  signal daq_run_state    : std_logic;
-  signal daq_gate_state   : std_logic;
-  signal veto_state       : std_logic;
-
-  -- DAQ gate number --
-  signal daq_gate_number  : GateNumberType;
-
-  -- Heartbeat counter --
-  signal hb_frame         : std_logic_vector(kWidthHBFrame-1 downto 0);
-  signal hb_count         : std_logic_vector(kWidthHBCount-1 downto 0);
-
   -- Heartbeat delimiter --
   signal hb_delimiter_wren        : std_logic;
-  signal hb_delimiter_1st_data    : std_logic_vector(kWidthData-1 downto 0);
-  signal hb_delimiter_2nd_data    : std_logic_vector(kWidthData-1 downto 0);
-
-  -- DAQ gate delimiter --
-  signal gate_delimiter_wren      : std_logic;
-  signal gate_delimiter_1st_data  : std_logic_vector(kWidthData-1 downto 0);
-  signal gate_delimiter_2nd_data  : std_logic_vector(kWidthData-1 downto 0);
-  signal gate_delimiter_ack       : std_logic;
-
-  -- second delimiter --
-  signal delimiter_2nd_wren       : std_logic;
-  signal delimiter_2nd_data       : std_logic_vector(kWidthData-1 downto 0);
+  signal hb_delimiter             : std_logic_vector(dOutDelimiter'range);
 
   -- flag --
-  signal global_hbf_num_mismatch  : std_logic;
-  signal reg_itt2_is_on           : std_logic;
+  signal reg_flags                : std_logic_vector(flagsIn'range);
+  signal edge_wren                : std_logic_vector(1 downto 0);
 
   -- Debug --
   attribute mark_debug : boolean;
-  attribute mark_debug of daq_run_state       : signal is enDEBUG;
-  attribute mark_debug of daq_gate_state      : signal is enDEBUG;
-  attribute mark_debug of veto_state          : signal is enDEBUG;
-
-  attribute mark_debug of delimiter_2nd_wren   : signal is enDEBUG;
-  attribute mark_debug of gate_delimiter_wren  : signal is enDEBUG;
-  attribute mark_debug of hb_delimiter_wren    : signal is enDEBUG;
-  attribute mark_debug of is_working           : signal is enDEBUG;
-  attribute mark_debug of valid_delimiter      : signal is enDEBUG;
-  --attribute mark_debug of final_delimiter_data : signal is enDEBUG;
+  attribute mark_debug of hb_delimiter_wren : signal is enDEBUG;
+  attribute mark_debug of hb_delimiter      : signal is enDEBUG;
+  attribute mark_debug of reg_flags         : signal is enDEBUG;
+  attribute mark_debug of flagsIn           : signal is enDEBUG;
 
 begin
   -- =========================== body ===============================
 
-  gHbfNumMismatchOut  <= global_hbf_num_mismatch;
-  runState            <= daq_run_state;
-  gateState           <= daq_gate_state;
-  vetoState           <= veto_state;
-
-  validDelimiter      <= valid_delimiter;
-  dOutDelimiter       <= final_delimiter_data;
-  isWorkingOut        <= is_working;
-
-  hbCount                 <=  hb_count;
-  valid_delimiter         <=  '1' when (delimiter_2nd_wren = '1' or hb_delimiter_wren = '1' or gate_delimiter_wren = '1') else '0';
-  final_delimiter_data    <=  delimiter_2nd_data        when( delimiter_2nd_wren = '1') else
-                              hb_delimiter_1st_data     when( hb_delimiter_wren ='1' ) else
-                              gate_delimiter_1st_data   when( gate_delimiter_wren = '1') else (others=>'0');
-  gen_tdc       : if kWorkingType = "TDC" generate begin
-    is_working            <=  '1' when (daq_run_state='1' and daq_gate_state='1' and veto_state='0' and lHbfNumMismatchIn='0' and tcpActive='1') else '0';
-  end generate;
-  gen_mikumari  : if kWorkingType = "MIKUMARI" generate begin
-    is_working            <=  '1' when (daq_run_state='1' and daq_gate_state='1' and tcpActive='1' ) else '0';
-  end generate;
-
-  -- Daq_run_state
-  u_run_process : process(sync_reset,clk)
-  begin
-    if(sync_reset = '1') then
-      daq_run_state <= '0';
-    elsif(clk'event and clk = '1') then
-      if(stcpCommand(kRunStart.id) = '1')then
-        daq_run_state <= '1';
-      elsif(stcpCommand(kRunEnd.id) = '1')then
-        daq_run_state <= '0';
-      end if;
-    end if;
-  end process;
-
-  -- Daq_gate_state
-  u_gate_process : process(sync_reset,clk)
-  begin
-    if(sync_reset = '1') then
-      daq_gate_state  <= '0';
-    elsif(clk'event and clk = '1') then
-      if(stcpPulse(kGateStart.id ) ='1')then
-        daq_gate_state  <= '1';
-      elsif(stcpPulse(kGateEnd.id) = '1')then
-        daq_gate_state  <= '0';
-      end if;
-    end if;
-  end process;
-
-  -- Veto_state
-  u_veto_process : process(sync_reset,clk)
-  begin
-    if(sync_reset = '1') then
-      veto_state <= '0';
-    elsif(clk'event and clk = '1') then
-      if(stcpPulse(kVetoStart.id) = '1')then
-        veto_state <= '1';
-      elsif(stcpPulse(kVetoEnd.id) = '1')then
-        veto_state <= '0';
-      end if;
-    end if;
-  end process;
-
-  -- DAQ gate number
-  u_gate_number_process : process(sync_reset,clk)
-  begin
-    if(sync_reset = '1') then
-      daq_gate_number  <= (others=>'0');
-    elsif(clk'event and clk = '1') then
-      if(stcpCommand(kGateNum.id) = '1')then
-        daq_gate_number  <= gateNumber;
-      end if;
-    end if;
-  end process;
-
-  -- Heartbeat counter
-  u_counter_process : process(sync_reset,clk)
-  begin
-    if(sync_reset = '1') then
-      hb_frame <= (others=>'0');
-      hb_count <= (others=>'0');
-    elsif(clk'event and clk = '1') then
-      if(stcpPulse(kHbCounterReset.id) = '1')then
-        hb_frame <= (others=>'0');
-        hb_count <= (others=>'0');
-      else
-        hb_count <= std_logic_vector(unsigned(hb_count) + 1);
-        -- Heartbeat!!! --
-        if(hb_count = kMaxHBCount)then
-          hb_frame <= std_logic_vector(unsigned(hb_frame) + 1);
-        end if;
-      end if;
-    end if;
-  end process;
+  validDelimiter      <= hb_delimiter_wren;
+  dOutDelimiter       <= hb_delimiter;
 
   -- Generate heartbeat delimiter
-  u_hb_delimiter_delimiter_process : process(sync_reset,clk)
+  u_delimiter_gen : process(sync_reset,clk)
   begin
     if(sync_reset = '1') then
       hb_delimiter_wren       <= '0';
-      hb_delimiter_1st_data   <= (others => '0');
-      hb_delimiter_2nd_data   <= (others => '0');
     elsif(clk'event and clk = '1') then
       -- Generate two delimiter words, 1st at kMaxHBCount-1, 2nd at KmaxHBCount --
-      if(unsigned(hb_count) >= unsigned(kMaxHBCount)-1 and daq_gate_state = '1' and daq_run_state = '1' and tcpActive = '1')then
-        hb_delimiter_wren  <= '1';
-        hb_delimiter_1st_data(kMSBDataType    downto kLSBDataType)  <= kDatatypeHeartbeat;
-        hb_delimiter_1st_data(kMSBFlag        downto kLSBFlag)      <= genFlagVector(kIndexGHbfNumMismatch, global_hbf_num_mismatch) or genFlagVector(kIndexInThrottlingT2, reg_itt2_is_on);
-        hb_delimiter_1st_data(kMSBGateNumber  downto kLSBGateNumber)<= daq_gate_number;
-        hb_delimiter_1st_data(kMSBHBFrame     downto kLSBHBFrame)   <= hb_frame;
-        hb_delimiter_2nd_data(kMSBDataType    downto kLSBDataType)  <= kDatatypeHeartbeat;
-        hb_delimiter_2nd_data(kMSBFlag        downto kLSBFlag)      <= genFlagVector(kIndexGHbfNumMismatch, global_hbf_num_mismatch) or genFlagVector(kIndexInThrottlingT2, reg_itt2_is_on);
-        hb_delimiter_2nd_data(kMSBGateNumber  downto kLSBGateNumber)<= daq_gate_number;
-        hb_delimiter_2nd_data(kMSBHBFrame     downto kLSBHBFrame)   <= hb_frame;
+      if(unsigned(hbCount) >= unsigned(kMaxHBCount)-1)then
+        hb_delimiter_wren                     <= '1';
+        hb_delimiter(kPosHbdDataType'range)   <= kDatatypeHeartbeat;
+        hb_delimiter(kPosHbdReserve1'range)   <= (others => '0');
+        hb_delimiter(kPosHbdFlag'range)       <= reg_flags;
+        hb_delimiter(kPosHbdOffset'range)     <= std_logic_vector(LaccpFineOffset);
+        hb_delimiter(kPosHbdHBFrame'range)    <= hbfNumber;
       else
-        hb_delimiter_wren       <= '0';
-        hb_delimiter_1st_data   <= (others => '0');
-        hb_delimiter_2nd_data   <= (others => '0');
+        hb_delimiter_wren                     <= '0';
       end if;
     end if;
   end process;
 
-  -- Generate gate delimiter
-  gate_delimiter_process : process(sync_reset,clk)
+  -- Flag record --
+  u_flag_recoard : process(sync_reset, clk)
   begin
     if(sync_reset = '1') then
-      gate_delimiter_wren       <= '0';
-      gate_delimiter_1st_data   <= (others => '0');
-      gate_delimiter_2nd_data   <= (others => '0');
+      reg_flags  <= (others => '0');
     elsif(clk'event and clk = '1') then
-      if(gate_delimiter_wren = '1' and gate_delimiter_ack = '1') then
-        gate_delimiter_wren       <= '0';
-        gate_delimiter_1st_data   <= (others => '0');
-        gate_delimiter_2nd_data   <= (others => '0');
-      elsif(stcpPulse(kGateStart.id) = '1' and daq_run_state = '1' and tcpActive = '1') then  -- daq_gate_state on
-        gate_delimiter_wren  <= '1';
-        gate_delimiter_1st_data(kMSBDataType    downto kLSBDataType)    <= kDatatypeGateOn;
-        gate_delimiter_1st_data(kMSBFlag        downto kLSBFlag)        <= genFlagVector(kIndexGHbfNumMismatch, global_hbf_num_mismatch) or genFlagVector(kIndexInThrottlingT2, reg_itt2_is_on);
-        gate_delimiter_1st_data(kMSBGateNumber  downto kLSBGateNumber)  <= daq_gate_number;
-        gate_delimiter_1st_data(kMSBHBCount     downto kLSBHBCount)     <= hb_frame;
-        gate_delimiter_2nd_data(kMSBDataType    downto kLSBDataType)    <= kDatatypeGateOn;
-        gate_delimiter_2nd_data(kMSBFlag        downto kLSBFlag)        <= genFlagVector(kIndexGHbfNumMismatch, global_hbf_num_mismatch) or genFlagVector(kIndexInThrottlingT2, reg_itt2_is_on);
-        gate_delimiter_2nd_data(kMSBGateNumber  downto kLSBGateNumber)  <= daq_gate_number;
-        gate_delimiter_2nd_data(kMSBHBCount     downto kLSBHBCount)     <= hb_count;
-      elsif(stcpPulse(kGateEnd.id) = '1' and daq_run_state = '1' and tcpActive = '1')then     -- daq_gate_state off
-        gate_delimiter_wren  <= '1';
-        gate_delimiter_1st_data(kMSBDataType    downto kLSBDataType)    <= kDatatypeGateOff;
-        gate_delimiter_1st_data(kMSBFlag        downto kLSBFlag)        <= genFlagVector(kIndexGHbfNumMismatch, global_hbf_num_mismatch) or genFlagVector(kIndexInThrottlingT2, reg_itt2_is_on);
-        gate_delimiter_1st_data(kMSBGateNumber  downto kLSBGateNumber)  <= daq_gate_number;
-        gate_delimiter_1st_data(kMSBHBCount     downto kLSBHBCount)     <= hb_frame;
-        gate_delimiter_2nd_data(kMSBDataType    downto kLSBDataType)    <= kDatatypeGateOff;
-        gate_delimiter_2nd_data(kMSBFlag        downto kLSBFlag)        <= genFlagVector(kIndexGHbfNumMismatch, global_hbf_num_mismatch) or genFlagVector(kIndexInThrottlingT2, reg_itt2_is_on);
-        gate_delimiter_2nd_data(kMSBGateNumber  downto kLSBGateNumber)  <= daq_gate_number;
-        gate_delimiter_2nd_data(kMSBHBCount     downto kLSBHBCount)     <= hb_count;
-      end if;
-    end if;
-  end process;
+      edge_wren <= edge_wren(0) & hb_delimiter_wren;
 
-  -- Second delimiter data insert --
-  delimiter_second_process : process(sync_reset,clk)
-  begin
-    if(sync_reset = '1') then
-      delimiter_2nd_wren   <= '0';
-      delimiter_2nd_data  <= (others => '0');
-      gate_delimiter_ack  <= '0';
-    elsif(clk'event and clk = '1') then
-      if(delimiter_2nd_wren = '1') then      -- second delimiter word
-        delimiter_2nd_wren   <= '0';
-        delimiter_2nd_data  <= (others => '0');
-        gate_delimiter_ack  <= '0';
-      elsif(hb_delimiter_wren='1') then     -- heartbeat delimiter
-        delimiter_2nd_wren   <= '1';
-        delimiter_2nd_data  <= hb_delimiter_2nd_data;
-        gate_delimiter_ack  <= '0';
-      elsif(gate_delimiter_wren='1') then   -- daq_gate_state delimiter
-        delimiter_2nd_wren   <= '1';
-        delimiter_2nd_data  <= gate_delimiter_2nd_data;
-        gate_delimiter_ack  <= '1';
-      else                                  -- idle
-        delimiter_2nd_wren   <= '0';
-        delimiter_2nd_data  <= (others => '0');
-        gate_delimiter_ack  <= '0';
-      end if;
-    end if;
-  end process;
-
-  -- Global hbf misthmath flag --
-  flag_process : process(sync_reset,clk)
-  begin
-    if(sync_reset = '1') then
-      global_hbf_num_mismatch  <= '0';
-    elsif(clk'event and clk = '1') then
-      if(stcpPulse(kHbCounterReset.id) = '1') then
-        global_hbf_num_mismatch  <= '0';
-      elsif(stcpCommand(kHbFrameNum.id) = '1') then
-        if(hbfNumber = hb_frame)then
-          global_hbf_num_mismatch  <= '0';
-        else
-          global_hbf_num_mismatch  <= '1';
+      for i in 0 to kWidthDelimiterFlag-1 loop
+        if(flagsIn(i) = '1') then
+          reg_flags(i)  <= '1';
+        elsif(edge_wren = "10") then
+          reg_flags(i)  <= '0';
         end if;
-      end if;
-    end if;
-  end process;
-
-  -- Input Throttling Type2 Record --
-  u_itt2_recoard : process(sync_reset, clk)
-  begin
-    if(sync_reset = '1') then
-      reg_itt2_is_on  <= '0';
-    elsif(clk'event and clk = '1') then
-      if(inThrottlingT2On = '1') then
-        reg_itt2_is_on  <= '1';
-      elsif(delimiter_2nd_wren = '1') then
-        reg_itt2_is_on  <= '0';
-      else
-        null;
-      end if;
+      end loop;
     end if;
   end process;
 
